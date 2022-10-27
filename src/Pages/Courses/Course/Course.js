ada862effff4be0a3fdd603f90e3ea25fd307f52
SAhmed28 <sanjida1@ualberta.ca>
2022-10-27 07:09:25 -0600

    added course details page

--- src/Pages/Courses/Course/Course.js
+++ src/Pages/Courses/Course/Course.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { FaStar } from "react-icons/fa";
 
 
 const Course = ({ course }) => {
 
     console.log(course)
-    const {_id, title, description, image, rating } = course;
+    const { _id, title, description, image, rating, company } = course;
     return (
-
         <Col>
             <Card>
-                <Card.Img style={{height:'200px'}} variant="top" src={image} />
+                <Card.Img style={{ height: '200px' }} variant="top" src={image} />
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>
-                    {
-                        description.length > 250 ?
-                            <>{description.slice(0, 100) + '...'} <Link to={`/courses/${_id}`}>Read More</Link></>
-                            :
-                            description
-                    }
+                        {
+                            description.length > 250 ?
+                                <>{description.slice(0, 100) + '...'} <Link to={`/courses/${_id}`}>Read More</Link></>
+                                :
+                                description
+                        }
                     </Card.Text>
                 </Card.Body>
+                <Card.Footer className='d-flex justify-content-between'>
+                    <div d-flex align-items-center>
+                        <FaStar className='text-warning me-2'></FaStar>
+                        <span>{rating}</span>
+                    </div>
+                    <div d-flex align-items-center>
+                        <span>From: <b>{company}</b></span>
+                    </div>
+                </Card.Footer>
             </Card>
         </Col>
     );
--- src/Pages/Courses/CourseDetails/CourseDetails.js
+++ src/Pages/Courses/CourseDetails/CourseDetails.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useLoaderData, Link } from 'react-router-dom'
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const CourseDetails = () => {
+    const singleCourseDetails = useLoaderData();
+    const { _id, title, description, image, rating } = singleCourseDetails;
     return (
         <div>
-            <h2>Course Details page</h2>
+            <h2>Course Details:</h2>
+
+            <Row xs={1} md={1} className="g-4">
+                <Col>
+                    <Card>
+                        <Card.Img style={{ height: '350px' }} variant="top" src={image} />
+                        <Card.Body>
+                            <Card.Title>{title}</Card.Title>
+                            <Card.Text>
+                                {description}
+                            </Card.Text>
+                            <Link to={`/courses`}>
+                                <Button variant="primary">Go Back to Courses</Button>
+                            </Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
 };
